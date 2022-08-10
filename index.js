03ef47ff49ebefba81250f060ea29083b3178fdf
中村颯 <halloween_hayato2003@yahoo.co.jp>
2022-08-10 11:10:20 +0900

    条件分岐変更

--- index.js
+++ index.js
@@ -35,37 +35,35 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
   req.body.events.forEach((event) => {
       // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
       if (event.type == "message" && event.message.type == "text"){
-        console.log(event.message.text);
-        events_processed.push(bot.replyMessage(event.replyToken, {
-          type: "text",
-          text: "あなた「" + event.message.text + "」"
-        }));
-
-        // if (event.message.text=="1"||event.message.text=="2"||event.message.text=="3"||event.message.text=="4"){
-        //   user_old = event.message.text;
-        //   events_processed.push(bot.replyMessage(event.replyToken, {
-        //     type: "text",
-        //     text: "あなたは大学" + user_old + "年生ですか？次は性別。男/女"
-        //   }));
-        // }
-
-        // if (user_old != ""){
-        //   if (event.message.text=="男"||event.message.text=="女"){
-        //     user_gender = event.message.text;
-        //     events_processed.push(bot.replyMessage(event.replyToken, {
-        //       type: "text",
-        //       text: "あなたは" + user_gender + "性です。情報を登録します。"
-        //     }));
-        //     user_data.push({Old:user_old, Gender:user_gender});
-        //     console.log(user_data);
-        //     events_processed.push(bot.replyMessage(event.replyToken, {
-        //       type: "text",
-        //       text: "情報を登録しました。あなたは大学"+ user_old + "年生の" + user_gender + "性です"
-        //     }));
-        //     user_gender = "";
-        //     user_old = "";
-        //   }
-        // }
+        if (event.message.text=="1"||event.message.text=="2"||event.message.text=="3"||event.message.text=="4"){
+          user_old = event.message.text;
+          events_processed.push(bot.replyMessage(event.replyToken, {
+            type: "text",
+            text: "あなたは大学" + user_old + "年生ですか？次は性別。男/女"
+          }));
+        }else if (user_old != ""){
+          if (event.message.text=="男"||event.message.text=="女"){
+            user_gender = event.message.text;
+            events_processed.push(bot.replyMessage(event.replyToken, {
+              type: "text",
+              text: "あなたは" + user_gender + "性です。情報を登録します。"
+            }));
+            user_data.push({Old:user_old, Gender:user_gender});
+            console.log(user_data);
+            events_processed.push(bot.replyMessage(event.replyToken, {
+              type: "text",
+              text: "情報を登録しました。あなたは大学"+ user_old + "年生の" + user_gender + "性です"
+            }));
+            user_gender = "";
+            user_old = "";
+          }else{
+            console.log(event.message.text);
+            events_processed.push(bot.replyMessage(event.replyToken, {
+              type: "text",
+              text: "あなた「" + event.message.text + "」"
+            }));
+          }
+        }
       }
   });
 
